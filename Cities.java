0e3a2ba739c771f7c5c2cc462d72fcb62d5f4dfa
EC2 Default User <ec2-user@ip-10-0-0-11.ec2.internal>
2018-07-30 11:11:25 +0000

    commit for cities

---
+++ Cities.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * Class Cities
+ */
+public class Cities {
+	
+	public static void main(String[] args) {
+    	System.out.println("Hello from AWS Cloud9!");
+	}
+	
+	private static List<String> getCityList(){
+		
+		List<String> cityList = new ArrayList<String>();
+		cityList.add("Bangalore");
+		cityList.add("Delhi");
+		cityList.add("Madras");
+		cityList.add("Calcutta");
+		return cityList;
+		
+	}
+}
